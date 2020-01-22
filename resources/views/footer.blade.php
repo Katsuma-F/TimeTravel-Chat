f7d6ef0689eb149ff6435c8f93a3a1180acd1b2b
Katsuma-F <k.fujita.indigo1003@gmail.com>
2020-01-22 23:23:56 +0900

    Add header and footer in @index

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,12 @@
+@section('footer')
+<footer>
+    <ul class="footer-menu">
+        <li>Home  | </li>
+        <li>About  | </li>
+        <li>Service  | </li>
+        <li>Contact Us  | </li>
+    </ul>
+    <p>© All rights reserved by TimeTravel-Chat.</p>
+</footer>
+
+@endsection
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,20 +1,29 @@
 @section('navbar')
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <a class="navbar__brand navbar__mainLogo" href="/"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-md-auto align-items-center">
-                    <li>
-                        <a class="btn btn-primary" href="/posts/new">投稿</a>
-                    </li>
-                    <li>
-                        <a class="nav-link commonNavIcon profile-icon" href="#"></a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03"
+        aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('/images/logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="メインロゴ">
+            TimeTravel-Chat
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ログイン </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">新規登録</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="キーワード" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
+
 @endsection
